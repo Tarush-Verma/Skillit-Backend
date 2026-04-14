bd99d5f71fc0a30e978a5f91e91867671495222a
Tarush-Verma <tarushverma13@gmail.com>
2026-04-15 00:37:46 +0530

    Removed node_modules and added gitignore

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/internships", require("./routes/internshipRoutes"));
+app.use("/api/hackathons", require("./routes/hackathonRoutes"));
+
+// MongoDB Connection (Atlas)
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+
+    // Start server ONLY after DB connects
+    const PORT = 5000;
+    app.listen(PORT, () =>
+      console.log(`🚀 Server running on port ${PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.log("❌ MongoDB Error:", err);
+  });
