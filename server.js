b0f697319840039a2050c4ca08b01dce2a0ca854
Tarush-Verma <tarushverma13@gmail.com>
2026-04-16 12:00:42 +0530

    fix port for deployment

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose
     console.log("✅ MongoDB Connected");
 
     // Start server ONLY after DB connects
-    const PORT = 5000;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () =>
       console.log(`🚀 Server running on port ${PORT}`)
     );
